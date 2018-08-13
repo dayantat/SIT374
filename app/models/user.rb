0965864452e25a05f090fc9b070633286bf123e4
Dayan Tatlock <dane@Dayans-MacBook-Pro.local>
2018-08-13 17:10:13 +1000

    Validation for name and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    validates :name, presence: true #this makes sure the entry cannot be blank
+    validates :email, presence: true #this makes sure the entry cannot be blank
 end
