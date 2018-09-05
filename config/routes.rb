ba23985e8d6221f6bde743f7c5deace20aee72e4
Dayan Tatlock <dane@Dayans-MacBook-Pro.local>
2018-09-05 20:53:05 +1000

    dropdown menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,22 @@
 Rails.application.routes.draw do
+#Get Requests
+
+  get 'sessions/new'
   get 'users/new'
     root 'static_pages#home'
     get '/help', to: 'static_pages#help'
     get '/about', to: 'static_pages#about'
     get '/contact', to: 'static_pages#contact'
     get '/signup', to: 'users#new'
+    get '/login', to: 'sessions#new'
+
+#Post Requests
     post '/signup', to: 'users#create'
+    post '/login', to: 'sessions#create'
+
+#Delete Requests
+
+    delete '/logout', to: 'sessions#destroy'
     resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
