cabb679b0e582d77a853ffcb3305b83690edf837
Dayan Tatlock <dane@Dayans-MacBook-Pro.local>
2018-09-07 23:52:33 +1000

    test 5

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module SampleApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.secret_key_base = 'e738ffedcc224ee3afe37ad82be229f621de76a7322b1fb996e60f4ae79a496f8f42312d00dba13337e0c7718eeeae008c306c6ba7cf4cc89867d9817f70e14e'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
