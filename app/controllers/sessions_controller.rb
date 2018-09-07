4fb71ddfb38a03e85bc9a4ecdbc144cd88a94b6d
Dayan Tatlock <dane@Dayans-MacBook-Pro.local>
2018-09-07 22:48:55 +1000

    Setup of Advanced login features

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,16 @@ class SessionsController < ApplicationController
     end
 
     def create
-        user = User.find_by(email: params[:session][:email].downcase)
-        if user && user.authenticate(params[:session][:password])
+        @user = User.find_by(email: params[:session][:email].downcase)
+        if @user && @user.authenticate(params[:session][:password])
             # Log the user in and redirect to the user's show page.
-            log_in(user) #this code has added bracket listing 8.30
-            redirect_to user
+            log_in(@user) #this code has added bracket listing 8.30
+            if params[:session][:remember_me] == '1'
+                remember(@user)
+            else
+                forget(@user)
+            end
+            redirect_to @user
         else
             flash.now[:danger] = 'Invalid email/password combination'
             render 'new'
@@ -16,7 +21,7 @@ class SessionsController < ApplicationController
     end
 
     def destroy
-        log_out
+        log_out if logged_in?
         redirect_to root_url
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    attr_accessor :remember_token
     before_save { self.email = email.downcase }
     validates(:name, presence: true, length: {maximum: 50})
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -13,4 +14,27 @@ class User < ApplicationRecord
 # If error occurs here read page 462 Listing 8.21
         BCrypt::Password.create(string, cost: cost)
     end
+
+    def User.new_token
+        SecureRandom.urlsafe_base64
+    end
+
+    #remembers a user in the DB this is used for persistent sessions.
+    def remember
+        self.remember_token = User.new_token
+        update_attribute(:remember_digest, User.digest(remember_token))
+    end
+
+    def authenticated?(remember_token)
+        if remember_digest.nil?
+            false
+        else
+            BCrypt::Password.new(remember_digest).is_password?(remember_token)
+        end
+    end
+
+    def forget
+        update_attribute(:remember_digest, nil)
+    end
+
 end
